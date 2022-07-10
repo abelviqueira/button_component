f0d91b191cba2632aa72c0b42cc101d7844f2099
abelviqueira <abelviqueira@gmail.com>
2022-07-11 01:35:26 +0200

    wip(Button): added some states

--- button_component/src/App.js
+++ button_component/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import Button from './components/Button';
 
@@ -6,7 +5,61 @@ function App() {
   return (
     <div className="App">
       <h1>Buttons</h1>
-      <Button />
+      <div className="buttons">
+        <div className='button'>
+          <p>{`<Button />`}</p>
+          <Button />
+        </div>
+        <div className='button'>
+          <p>{`&:hover, &:focus`}</p>
+          <Button hover/>
+        </div>
+      </div>
+      <div className="buttons">
+        <div className='button'>
+          <p>{`<Button variant="outline" />`}</p>
+          <Button variant="outline">Outline</Button>
+        </div>
+        <div className='button'>
+          <p>{`&:hover, &:focus`}</p>
+          <Button variant="outline" hover>Outline</Button>
+        </div>
+      </div>
+      <div className="buttons">
+        <div className='button'>
+          <p>{`<Button variant="outline" />`}</p>
+          <Button variant="text">Text</Button>
+        </div>
+        <div className='button'>
+          <p>{`&:hover, &:focus`}</p>
+          <Button variant="text" hover>Text</Button>
+        </div>
+      </div>
+      <div className="buttons">
+        <div className='button'>
+          <p>{`<Button disableShadow />`}</p> 
+          <Button disableShadow>Disable Shadow</Button>
+        </div>
+      </div>
+      <div className="buttons">
+        <div className='button'>
+          <p>{`<Button disabled />`}</p> 
+          <Button disabled>Disabled</Button>
+        </div>
+        <div className='button'>
+          <p>{`<Button variant="text" disabled />`}</p> 
+          <Button variant="text" disabled>Disabled</Button>
+        </div>
+      </div>
+      <Button startIcon="local_grocery_store">Start Icon</Button>
+      <Button endIcon="local_grocery_store">End Icon</Button>
+      <Button size="sm">Size Small</Button>
+      <Button size="md">Size Medium</Button>
+      <Button size="lg">Size Large</Button>
+      <Button color="default">Default</Button>
+      <Button color="primary">Primary</Button>
+      <Button color="secondary">Secondary</Button>
+      <Button color="danger">Danger</Button>
     </div>
   );
 }
--- button_component/src/components/Button/index.js
+++ button_component/src/components/Button/index.js
@@ -1,12 +1,29 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import './style.css';
-
 class Button extends Component {
+    constructor(props) {
+        super(props);
+        let variant = typeof this.props.variant !== 'undefined' ? this.props.variant : false;
+        let disabled = typeof this.props.disabled !== 'undefined' ? 'disabled' : false;
+        let disableShadow = typeof this.props.disableShadow !== 'undefined' ? 'disableShadow' : false;
+
+        this.classes = {
+            variant: variant,
+            disabled: disabled,
+            disableShadow: disableShadow,
+        }
+        
+    }
+    
+
     render() {
+        // const classes = this.state.variant + ' ' + this.state.disabled + ' ' + this.state.disableShadow;
+        const classes = Object.values(this.classes).filter((x) => x !== false).join(' ');
+
         return (
-            <div className='button'>
+            <button key="button" className={classes}>
                 {this.props.children ? this.props.children : 'Default'}
-            </div>
+            </button>
         );
     }
 }
